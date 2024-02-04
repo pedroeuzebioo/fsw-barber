809f3e17e94c7aebb0a535d320d779bdf1230a12
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-02-04 18:30:22 -0300

    feat: add SideMenu component

--- src/app/barbershops/[id]/_components/barbershop-info.tsx
+++ src/app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,22 +1,20 @@
 "use client";
 
+import SideMenu from "@/components/side-menu";
 import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Barbershop } from "@prisma/client";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-
 interface BarbershopInfoProps {
   barbershop: Barbershop;
 }
-
 const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
   const router = useRouter();
-
   const handleBackClick = () => {
     router.back();
   };
-
   return (
     <div>
       <div className="h-[250px] w-full relative">
@@ -24,9 +22,17 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
           <ChevronLeftIcon />
         </Button>
 
-        <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
-          <MenuIcon />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
 
         <Image
           src={barbershop.imageUrl}
@@ -38,7 +44,6 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
           className="opacity-75"
         />
       </div>
-
       <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
         <h1 className="text-xl font-bold">{barbershop.name}</h1>
         <div className="flex items-center gap-1 mt-2">
@@ -53,5 +58,4 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
     </div>
   );
 };
-
 export default BarbershopInfo;
