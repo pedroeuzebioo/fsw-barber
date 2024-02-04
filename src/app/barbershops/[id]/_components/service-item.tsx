522e002801e512f169a750081aa3186ed9594e26
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-02-04 19:05:14 -0300

    feat: ensure a service can be booked by the user

--- src/app/barbershops/[id]/_components/service-item.tsx
+++ src/app/barbershops/[id]/_components/service-item.tsx
@@ -1,16 +1,17 @@
 "use client";
-
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Card, CardContent } from "@/components/ui/card";
 import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { Barbershop, Service } from "@prisma/client";
 import { ptBR } from "date-fns/locale";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useMemo, useState } from "react";
 import { generateDayTimeList } from "../_helpers/hours";
-import { format } from "date-fns";
+import { format, setHours, setMinutes } from "date-fns";
+import { saveBooking } from "../_actions/save-booking";
+import { Loader2 } from "lucide-react";
 
 interface ServiceItemProps {
   barbershop: Barbershop;
@@ -19,29 +20,54 @@ interface ServiceItemProps {
 }
 
 const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps) => {
+  const { data } = useSession();
+
   const [date, setDate] = useState<Date | undefined>(undefined);
   const [hour, setHour] = useState<string | undefined>();
+  const [submitIsLoading, setSubmitIsLoading] = useState(false);
 
   const handleDateClick = (date: Date | undefined) => {
     setDate(date);
     setHour(undefined);
   };
-
   const handleHourClick = (time: string) => {
     setHour(time);
   };
-
   const handleBookingClick = () => {
     if (!isAuthenticated) {
       return signIn("google");
     }
-    // TODO: abrir modal de agendamento
+  };
+
+  const handleBookingSubmit = async () => {
+    setSubmitIsLoading(true);
+
+    try {
+      if (!hour || !date || !data?.user) {
+        return;
+      }
+
+      const dateHour = Number(hour.split(":")[0]);
+      const dateMinutes = Number(hour.split(":")[1]);
+
+      const newDate = setMinutes(setHours(date, dateHour), dateMinutes);
+
+      await saveBooking({
+        serviceId: service.id,
+        barbershopId: barbershop.id,
+        date: newDate,
+        userId: (data.user as any).id,
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setSubmitIsLoading(false);
+    }
   };
 
   const timeList = useMemo(() => {
     return date ? generateDayTimeList(date) : [];
   }, [date]);
-
   return (
     <Card>
       <CardContent className="p-3 w-full">
@@ -71,12 +97,10 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
                     Reservar
                   </Button>
                 </SheetTrigger>
-
                 <SheetContent className="p-0">
                   <SheetHeader className="text-left px-5 py-6 border-b border-solid border-secondary">
                     <SheetTitle>Fazer Reserva</SheetTitle>
                   </SheetHeader>
-
                   <div className="py-6">
                     <Calendar
                       mode="single"
@@ -109,7 +133,6 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
                       }}
                     />
                   </div>
-
                   {/* Mostrar lista de horários apenas se alguma data estiver selecionada */}
                   {date && (
                     <div className="flex gap-3 overflow-x-auto py-6 px-5 border-t border-solid border-secondary [&::-webkit-scrollbar]:hidden">
@@ -125,7 +148,6 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
                       ))}
                     </div>
                   )}
-
                   <div className="py-6 px-5 border-t border-solid border-secondary">
                     <Card>
                       <CardContent className="p-3 gap-3 flex flex-col">
@@ -139,7 +161,6 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
                             }).format(Number(service.price))}
                           </h3>
                         </div>
-
                         {date && (
                           <div className="flex justify-between">
                             <h3 className="text-gray-400 text-sm">Data</h3>
@@ -150,14 +171,12 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
                             </h4>
                           </div>
                         )}
-
                         {hour && (
                           <div className="flex justify-between">
                             <h3 className="text-gray-400 text-sm">Horário</h3>
                             <h4 className="text-sm">{hour}</h4>
                           </div>
                         )}
-
                         <div className="flex justify-between">
                           <h3 className="text-gray-400 text-sm">Barbearia</h3>
                           <h4 className="text-sm">{barbershop.name}</h4>
@@ -167,7 +186,10 @@ const ServiceItem = ({ service, barbershop, isAuthenticated }: ServiceItemProps)
                   </div>
 
                   <SheetFooter className="px-5">
-                    <Button disabled={!hour || !date}>Confirmar reserva</Button>
+                    <Button onClick={handleBookingSubmit} disabled={!hour || !date || submitIsLoading}>
+                      {submitIsLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+                      Confirmar reserva
+                    </Button>
                   </SheetFooter>
                 </SheetContent>
               </Sheet>
