118028582b89db6cf00a3f87525b8d9f8de13996
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-02-04 19:43:17 -0300

    feat: ensure confirmed bookings are shown on home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,9 +5,30 @@ import Search from "./_components/search";
 import BookingItem from "@/components/booking-item";
 import { db } from "@/lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/api/auth/[...nextauth]/route";
+
 export default async function Home() {
-  // chamar prisma e pegar barbearias
-  const barbershops = await db.barbershop.findMany({});
+  const session = await getServerSession(authOptions);
+
+  const [barbershops, confirmedBookings] = await Promise.all([
+    db.barbershop.findMany({}),
+    session?.user
+      ? db.booking.findMany({
+          where: {
+            userId: (session.user as any).id,
+            date: {
+              gte: new Date(),
+            },
+          },
+          include: {
+            service: true,
+            barbershop: true,
+          },
+        })
+      : Promise.resolve([]),
+  ]);
+
   return (
     <div>
       <Header />
@@ -22,10 +43,17 @@ export default async function Home() {
       <div className="px-5 mt-6">
         <Search />
       </div>
-      {/* <div className="px-5 mt-6">
-        <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
-        <BookingItem />
-      </div> */}
+
+      <div className="mt-6">
+        <h2 className="pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+
+        <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {confirmedBookings.map((booking) => (
+            <BookingItem key={booking.id} booking={booking} />
+          ))}
+        </div>
+      </div>
+
       <div className="mt-6">
         <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
@@ -34,10 +62,8 @@ export default async function Home() {
           ))}
         </div>
       </div>
-
       <div className="mt-6 mb-[4.5rem]">
         <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
-
         <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
